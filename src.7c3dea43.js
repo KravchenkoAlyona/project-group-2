8bdcad2b7321847ae0cffc6f18a06e48d9900cdf
Levchenko66 <81429805+Levchenko66@users.noreply.github.com>
2021-07-04 10:58:48 +0000

    Deploying to gh-pages from @ Levchenko66/project-group-2@ad997bacd45e3b3be97e99550e61331f2bd5ea54 🚀

--- src.7c3dea43.js
+++ src.7c3dea43.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-group-2/src.270342bb.js.map
+//# sourceMappingURL=/project-group-2/src.7c3dea43.js.map
