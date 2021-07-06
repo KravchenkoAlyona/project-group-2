52868c5fc8b4cd9717210307207c0911a62555f1
Levchenko66 <81429805+Levchenko66@users.noreply.github.com>
2021-07-06 05:30:15 +0000

    Deploying to gh-pages from @ Levchenko66/project-group-2@833e2ec67cffb499683b524caa5b20f55f98142b 🚀

--- src.54353950.js
+++ src.54353950.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/hero/background/bg-mobile.jpg":[["bg-mobile.fb27e8ab.jpg","ry61"],"ry61"],"./../images/hero/background/bg-tablet.jpg":[["bg-tablet.581545ec.jpg","gadN"],"gadN"],"./../images/svg/advantages-student.svg":[["advantages-student.667be231.svg","FeN5"],"FeN5"],"./../images/svg/advantages-mobile_learning.svg":[["advantages-mobile_learning.f2b465a3.svg","eYYz"],"eYYz"],"./../images/svg/advantages-insurance.svg":[["advantages-insurance.c9786a65.svg","pmZV"],"pmZV"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-group-2/src.d282f918.js.map
+//# sourceMappingURL=/project-group-2/src.54353950.js.map
