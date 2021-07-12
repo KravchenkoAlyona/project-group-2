371c39a0252721b1860227cccff77fcc23d44796
Danylo Nehreba <butanasiland@gmail.com>
2021-07-13 01:17:11 +0200

    slider

---
+++ src/js/swiper.js
@@ -0,0 +1,11 @@
+new Swiper('.reviews-slider', {
+    // стрілки
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    centeredSlides: true,
+    speed: 1000,
+   
+
+});
